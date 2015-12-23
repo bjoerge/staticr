3623cb36231241d0fb5c297f7f2cd7c6f0e2bc16
Bjørge Næss <bjoerge@gmail.com>
2015-12-23 19:10:58 +0100

    Update deps for example project

--- example/static-routes/sass-bundles.js
+++ example/static-routes/sass-bundles.js
@@ -4,12 +4,16 @@ var sass = require('node-sass');
 // This bundle will be minified in production.
 var routes = module.exports = {
   "/css/main.css": function(callback) {
-    sass.render({
-      file: require.resolve("../stylesheets/main.scss"),
-      success: callback.bind(null, null),
-      error: function(errmsg) {
-        callback(new Error(errmsg));
+    var options = {
+      file: require.resolve('../stylesheets/main.scss'),
+      omitSourceMapUrl: true,
+      outputStyle: 'nested'
+    }
+    sass.render(options, function (err, result) {
+      if (err) {
+        return callback(err)
       }
-    });
+      callback(null, result.css)
+    })
   }
 };
